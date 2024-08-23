cc1b9059a33fe645e1e124efec4c36b2656190ae
darshan.magdum <darshan.magdum@nihilent.com>
2024-08-23 10:21:21 +0530

    f

--- FrontEnd/src/Screens/Landingpage/ContactUs.js
+++ FrontEnd/src/Screens/Landingpage/ContactUs.js
@@ -4,20 +4,71 @@ import ContactImage from '../../../../FrontEnd/src/Images/ContactImage.png';
 
 const ContactUs = () => {
   return (
-    <div className="outer-container">
-      <h1 className="heading">Contact Us</h1>
-      <div className="content-container">
-        <div className="image" style={{ backgroundImage: `url(${ContactImage})` }}></div>
-        <div className="form-container">
+    <section className="contact" id="contact">
+      <div className="container">
+        <div className="heading text-center">
+          <h2>
+            Contact <span>Us</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            <br />incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="row">
+          <div className="col-md-5">
+            <div className="title">
+              <h3>Contact Detail</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+            </div>
+            <div className="content">
+              {/* Info-1 */}
+              <div className="info">
+                <i className="fas fa-mobile-alt"></i>
+                <h4>
+                  PHONE :<br />
+                  <span>+12457836913 , +12457836913</span>
+                </h4>
+              </div>
+              {/* Info-2 */}
+              <div className="info">
+                <i className="far fa-envelope"></i>
+                <h4>
+                  EMAIL :<br />
+                  <span>example@info.com</span>
+                </h4>
+              </div>
+              {/* Info-3 */}
+              <div className="info">
+                <i className="fas fa-map-marker-alt"></i>
+                <h4>
+                  ADDRESS :<br />
+                  <span>6743 last street, Abcd, Xyz</span>
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-7">
           <form>
-            <input type="text" placeholder="Name" className="input" />
-            <input type="email" placeholder="Email" className="input" />
-            <textarea placeholder="Message" className="input textarea" />
-            <button type="submit" className="button">Send</button>
-          </form>
+          <div className="form-group">
+      <input type="text" className="form-control" placeholder="Name" />
+    </div>
+    <div className="form-group">
+      <input type="email" className="form-control" placeholder="Email" />
+    </div>
+  <div className="form-group">
+    <input type="text" className="form-control" placeholder="Subject" />
+  </div>
+  <div className="form-group">
+    <textarea className="form-control" rows="5" placeholder="Message"></textarea>
+  </div>
+  <button className="btn btn-block" type="submit">Send Now!</button>
+</form>
+
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
